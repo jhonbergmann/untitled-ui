a700654352d118e280f31be5137eb5efc18990a6
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2024-06-17 14:35:33 -0300

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  semi: false,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
