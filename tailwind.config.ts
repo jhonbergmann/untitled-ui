521aafce2e918cd371f7521c7c4335ef25ca2ada
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2024-06-17 14:36:26 -0300

    tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type {Config} from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+}
+export default config
